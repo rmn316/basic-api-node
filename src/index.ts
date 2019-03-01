397f1067c691e3b926128e910bd8000a9650ec35
Robert Neal <rmn316@gmail.com>
2019-03-01 20:10:55 +0700

    initial commit - typescript / node app.

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+import express from "express";
+
+// initialize configuration
+dotenv.config();
+
+const port = 3000; // process.env.SERVER_PORT;
+const app = express();
+
+app.get("/", (request, response) => {
+    response.send("Hello World!");
+});
+
+// start the Express server
+app.listen( port, () => {
+    // tslint:disable-next-line:no-console
+    console.log( `server started at http://localhost:${ port }` );
+});
